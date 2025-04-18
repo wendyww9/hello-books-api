b0f4fbfcdf0f9467b262c7a88fb8d61688394f32
wendyww9 <wendyskystar@gmail.com>
2025-04-17 19:38:09 -0700

    update GET with book_id and its responses

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
 
 books_bp = Blueprint("book_bp", __name__, url_prefix = "/books")
@@ -16,3 +16,26 @@ def get_all_books():
         )
     return books_response
 
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
