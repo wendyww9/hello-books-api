ce60c2a12ecfb4413a13ec62ac5d7bccc2ba2ffc
wendyww9 <wendyskystar@gmail.com>
2025-04-23 17:45:02 -0700

    add GET and POST method using SQLALchemy

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,41 +1,75 @@
-from flask import Blueprint, abort, make_response
-from app.models.book import books
+from flask import Blueprint, abort, make_response, request
+# from app.models.book import books
+from app.models.book import Book
+from ..db import db
 
 books_bp = Blueprint("book_bp", __name__, url_prefix = "/books")
 
-@books_bp.get("")
-def get_all_books():
-    books_response = []
-    for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
-    return books_response
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body['title']
+    description = request_body['description']
 
-@books_bp.get("/<book_id>")
-def get_one_book(book_id):
-    book = validate_book(book_id)
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description,
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
     }
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        response = {"message": f"book {book_id} invalid"}
-        abort(make_response(response, 400))
+    return response, 201
 
+@books_bp.get("")
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+
+    books_response = []
     for book in books:
-        if book.id == book_id:
-            return book
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return books_response
 
-    response = {"message": f"book {book_id} not found"}
-    abort(make_response(response, 404))
+# @books_bp.get("")
+# def get_all_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append(
+#             {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             }
+#         )
+#     return books_response
+
+# @books_bp.get("/<book_id>")
+# def get_one_book(book_id):
+#     book = validate_book(book_id)
+
+#     return {
+#         "id": book.id,
+#         "title": book.title,
+#         "description": book.description,
+#     }
+
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         response = {"message": f"book {book_id} invalid"}
+#         abort(make_response(response, 400))
+
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+
+#     response = {"message": f"book {book_id} not found"}
+#     abort(make_response(response, 404))
