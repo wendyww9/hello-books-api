1514be838722da5aa00662336e631ed44169b450
wendyww9 <wendyskystar@gmail.com>
2025-04-29 16:50:28 -0700

    add tests

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,19 @@ from flask import Flask
 from app.routes.book_routes import books_bp
 from .db import db, migrate
 from .models import book
+import os
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
     
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     
+    if config:
+        # Merge `config` into the app's configuration
+        # to override the app's default settings
+        app.config.update(config)
+
     db.init_app(app)
     migrate.init_app(app, db)
 
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 # from app.models.book import books
 from app.models.book import Book
 from ..db import db
@@ -25,9 +25,21 @@ def create_book():
 
 @books_bp.get("")
 def get_all_books():
-    query = db.select(Book).order_by(Book.id)
-    books = db.session.scalars(query)
+    # query = db.select(Book).order_by(Book.id)
+    # books = db.session.scalars(query)
+    query = db.select(Book)
 
+    title_param = request.args.get("title")
+    if title_param:
+        query = query.where(Book.title.ilike(f"%{title_param}%"))
+
+    description_param = request.args.get("description")
+    if description_param:
+        query = query.where(Book.description.ilike(f"%{description_param}%"))
+
+    query = query.order_by(Book.id)
+    books = db.session.scalars(query)
+    
     books_response = []
     for book in books:
         books_response.append({
@@ -36,6 +48,50 @@ def get_all_books():
             "description": book.description
         })
     return books_response
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
+
+def validate_book(book_id):
+    try: 
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 404))
+    
+    return book
+
+@books_bp.put("/<book_id>")
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body['title']
+    book.description = request_body['description']
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book = validate_book(book_id)
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
 
 # @books_bp.get("")
 # def get_all_books():
